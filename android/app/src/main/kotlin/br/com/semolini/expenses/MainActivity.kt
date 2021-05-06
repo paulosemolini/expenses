aa321d9f61bf609a3d3dc2b6947f60bf8e69e93b
Paulo Henrique <paulohenriquess@gmail.com>
2021-05-05 21:33:14 -0300

    Versão Inicial

---
+++ android/app/src/main/kotlin/br/com/semolini/expenses/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.semolini.expenses
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) => runApp(Expenses());
+
+class Expenses extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Despesas Pessoais'),
+      ),
+      body: Center(
+        child: Text('Versão Inicial.'),
+      ),
+    );
+  }
+}
